248f28eaa1b079627d185c5022696377dbce59a1
agyen <izagyen96@gmail.com>
2020-07-08 20:00:39 +0000

    data in table

--- main.py
+++ main.py
@@ -1,17 +1,74 @@
 import requests
+from collections import defaultdict
+import json
+
+from rich.console import Console
+from rich.table import Column, Table
+from collections import namedtuple
+from rich import box
+
 
 url = requests.get('https://employeeservice002.herokuapp.com/v2/api-docs')
 urlLink = url.json()
 
 description = urlLink['info']['description']
-print(description)
 
-a_dict = {}
-for rest in list(urlLink['paths'].keys()):
-    a_dict[rest] = list(urlLink['paths'][rest].keys())
-    args = {}
-    for summary in list(urlLink['paths'][rest].keys()):
-        args[summary] = urlLink['paths'][rest][summary]['summary']
-    a_dict[rest] = args
-print(a_dict)
+tagsList = []
+for tags in urlLink['tags']:
+    tagsList.append(tags['name'])
+
+finalList = []
+def getPaths():
+    url = requests.get('https://employeeservice002.herokuapp.com/v2/api-docs')
+    urlLink = url.json()
+    for key, value in urlLink['paths'].items():
+        getChildPath(key,value)
+
+    return None
+
+def getChildPath(parentkey, data):
+
+    for key, value in data.items():
+        shape = {}
+        shape['path']= parentkey
+        shape['request_methods']= key
+        shape['description']= value['summary']
+        shape['tag'] = value['tags'][0] 
+        final(shape)
     
+    return 
+
+def final(shape):
+    # for t in finalList:
+    #     print(t)
+    finalList.append(shape)
+    
+
+getPaths()
+# print(json.dumps(finalList, indent=4, sort_keys=True))
+
+
+console = Console()
+
+table = Table(show_header=True, header_style="bold magenta", title=description, title_style="green", box=box.HEAVY)
+table.add_column("Request Method")
+table.add_column("Path", justify="right")
+table.add_column("Description", justify="right")
+table.add_column("Tag", justify="right")
+
+print(finalList)
+for row in finalList:
+   
+    table.add_row(
+    row['request_methods'],
+    row['path'],
+    row['description'],
+    row['tag']
+    )
+
+console.print(table)
+    
+
+
+
+
