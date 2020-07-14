5880327a134036e0688b684f80390d85d59aae03
christianaasare <christiana.asare@turntabl.io>
2020-07-14 17:03:08 +0000

    --animation

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ from rich.console import Console
 from rich.table import Column, Table
 from collections import namedtuple
 from rich import box
-from contextlib import contextmanager
 from rich.measure import Measurement
 from rich.text import Text
-from contextlib import contextmanager
 
 
 url = requests.get('https://petstore.swagger.io/v2/swagger.json')
@@ -62,14 +60,8 @@ getPaths()
 
 console = Console()
 
-BEAT_TIME = 0.04
 
-@contextmanager
-def beat(length: int = 1) -> None:
-    with console:
-        console.clear()
-        yield
-    time.sleep(length * BEAT_TIME)
+
 
 
 table = Table(show_header=True, header_style="bold magenta", title_style="green", box=box.HEAVY, border_style="bright_green")
@@ -79,40 +71,31 @@ console.show_cursor(False)
 
 try:
     table.add_column("Request Method")
-    with beat(10):
-        console.print(table, justify="center")
+    console.print(table, justify="center")
 
     table.add_column("Path")
-    with beat(10):
-        console.print(table, justify="center")
+    console.print(table, justify="center")
 
     table.add_column("Description")
-    with beat(10):
-        console.print(table, justify="center")
+    console.print(table, justify="center")
 
     table.add_column("Controller")
-    with beat(10):
-        console.print(table, justify="center")
+    console.print(table, justify="center")
 
     table.add_column("Data Type")
-    with beat(10):
-        console.print(table, justify="center")
+    console.print(table, justify="center")
 
     table.title = description
-    with beat(10):
-        console.print(table, justify="center")
+    console.print(table, justify="center")
 
     table.caption = "Aftermath: turntabl swagger cli"
-    with beat(10):
-        console.print(table, justify="center")
+    console.print(table, justify="center")
 
     table.caption = "Aftermath: [b]turntabl swagger cli[/b]"
-    with beat(10):
-        console.print(table, justify="center")
+    console.print(table, justify="center")
 
     table.caption = "Aftermath: [b magenta not dim]turntabl swagger cli[/]"
-    with beat(10):
-        console.print(table, justify="center")
+    console.print(table, justify="center")
     
     # table_width = Measurement.get(console, table, console.width).maximum
 
@@ -137,8 +120,7 @@ try:
          row['tag'],
          row['data_type']
         )
-        with beat(10):
-            console.print(table, justify="center")
+        console.print(table, justify="center")
 
     # console.print(":smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up::smiley: :thumbs_up: :smiley: :thumbs_up::smiley: :thumbs_up: ",table,":thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley: :thumbs_up: :smiley:")
 
